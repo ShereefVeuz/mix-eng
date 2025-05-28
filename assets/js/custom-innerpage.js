df0402f34765d4e783db9141b9f4191b8a8129e3
shereef <shareef@veuz.in>
2025-05-28 20:00:49 +0530

    td2 tow commit

--- assets/js/custom-innerpage.js
+++ assets/js/custom-innerpage.js
@@ -730,6 +730,49 @@ gsap.from(".right-page-career-in-out", {
   }
 });
 
+
+
+// change text
+$(document).ready(function () {
+  var changebox = $(".changebox");
+
+  var firstclone = changebox.children(":first").clone();
+  changebox.append(firstclone);
+
+  var fsstr = changebox.parent().css("font-size");
+  fsstr = fsstr.slice(0, fsstr.indexOf("p"));
+  var fs = parseInt(fsstr);
+
+  changebox.css("height", changebox.parent().css("line-height"));
+  ChangeSize(0);
+  setInterval(Next, 2000);
+
+  function Next() {
+    if (typeof Next.i == 'undefined') {
+      Next.i = 0;
+    }
+    Next.i++;
+    if (Next.i == changebox.children("span").length) {
+      Next.i = 1;
+      changebox.scrollTop(0);
+    }
+    changebox.animate({
+      scrollTop: (fs * Next.i) + Next.i * 5 + 3
+    }, 500);
+    setTimeout(function () {
+      ChangeSize(Next.i);
+    }, 500);
+  }
+
+  function ChangeSize(i) {
+    var word = changebox.children("span").eq(i);
+    var wordsize = word.css("width");
+    changebox.css("width", wordsize);
+  }
+});
+
+
+
 // last career page reveal text
 // document.addEventListener("DOMContentLoaded", function () {
 //   const paragraph2 = document.getElementById("splitText");
@@ -817,49 +860,6 @@ filterContainer.addEventListener("click", (event) => {
     });
   }
 });
-
-
-
-// last added
-// change text
-$(document).ready(function () {
-  var changebox = $(".changebox");
-
-  var firstclone = changebox.children(":first").clone();
-  changebox.append(firstclone);
-
-  var fsstr = changebox.parent().css("font-size");
-  fsstr = fsstr.slice(0, fsstr.indexOf("p"));
-  var fs = parseInt(fsstr);
-
-  changebox.css("height", changebox.parent().css("line-height"));
-  ChangeSize(0);
-  setInterval(Next, 2000);
-
-  function Next() {
-    if (typeof Next.i == 'undefined') {
-      Next.i = 0;
-    }
-    Next.i++;
-    if (Next.i == changebox.children("span").length) {
-      Next.i = 1;
-      changebox.scrollTop(0);
-    }
-    changebox.animate({
-      scrollTop: (fs * Next.i) + Next.i * 5 + 3
-    }, 500);
-    setTimeout(function () {
-      ChangeSize(Next.i);
-    }, 500);
-  }
-
-  function ChangeSize(i) {
-    var word = changebox.children("span").eq(i);
-    var wordsize = word.css("width");
-    changebox.css("width", wordsize);
-  }
-});
-
 
 
 
